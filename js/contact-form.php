ff149eafb22795e3ccbf8074c8a330505ef067c7
Larry Weru <larryw09@gmail.com>
2016-03-23 04:21:42 -0400

    design

---
+++ js/contact-form.php
@@ -0,0 +1,5 @@
+<div id='success_page' class="text-center">
+    <h2 style="text-align: center;">Email Sent Successfully.</h2>
+    <p style="text-align: center;">Thank you! your message has been sent to us.</p>
+    <br />
+</div>
--- js/contactFormHandler.js
+++ js/contactFormHandler.js
@@ -1,19 +1,19 @@
 $(function() {
 
   toastr.options = {
-  "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": false,
-  "positionClass": "toast-top-right",
-  "preventDuplicates": true,
-  "onclick": null,
-  "timeOut": "50000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-}
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "timeOut": "50000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
 
   var successMsg = "Your message has been sent."; // Message shown on success.
   var failMsg = "Sorry it seems that our mail server is not responding, Sorry for the inconvenience!"; // Message shown on fail.
@@ -24,21 +24,26 @@ $(function() {
       event.preventDefault(); // prevent default submit behaviour
 
       var email = $form.find("input.email").val();
-      var campaign = $form.find("input.contact-campaign").val();
+      var name = $form.find("input.contact-name").val();
+      var phone = $form.find("input.contact-phone").val();
+      var message = $form.find("textarea.contact-message").val();
 
       // Google Sheets Email Insert (GET https://script.google.com/macros/s/AKfycbzVaB0QneZdlR323dU-XYJHuqOMF8rpiKluMlWzZijKu1RV5fS7/exec)
 
       jQuery.ajax({
-          url: "https://script.google.com/macros/s/AKfycbzVaB0QneZdlR323dU-XYJHuqOMF8rpiKluMlWzZijKu1RV5fS7/exec",
+          url: "https://script.google.com/macros/s/AKfycbySMTMYq2E56M_-WgCP-z2Pi-v0lpaNIeRZQramji0t_z_0eBQ/exec",
           type: "GET",
           data: {
-            "insert": "{\"SHEET_NAME\":\"Sheet1\",\"DATA\":[{\"email\":\"" + email + "\",\"campaign\":\"" + campaign + "\",\"date\":\"" + $.now() + "\"}]}",
+            "insert": "{\"SHEET_NAME\":\"Sheet1\",\"DATA\":[{\"email\":\"" + email + "\",\"date\":\"" + $.now() + "\"}]}",
+          },
+          data: {
+            "insert": "{\"SHEET_NAME\":\"contact_sheet\",\"DATA\":[{\"email\":\"" + email + "\",\"date\":\"" + $.now() + "\",\"name\":\"" + name + "\",\"phone\":\"" + phone + "\",\"message\":\"" + message + "\"}]}",
           }
         })
         .done(function(data, textStatus, jqXHR) {
           console.log("HTTP Request Succeeded: " + jqXHR.status);
           console.log(data);
-          toastr["success"]("Thank you. Look out for an email from us soon!");
+          // toastr["success"]("Thank you. Look out for an email from us soon!");
           // alert("Thank you! We've set up a ticket for you and will let you know when we've opened our doors"); // You might want to redirect the user here, or show a stylized message to them.
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
