838b1cfc6693f62888ac9e8c274b8a5dc184b518
Larry Weru <larryw09@gmail.com>
2016-04-14 17:19:21 -0400

    theme change

--- js/contact-form.php
+++
@@ -1,5 +0,0 @@
-<div id='success_page' class="text-center">
-    <h2 style="text-align: center;">Email Sent Successfully.</h2>
-    <p style="text-align: center;">Thank you! your message has been sent to us.</p>
-    <br />
-</div>
